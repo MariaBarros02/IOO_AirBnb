b67a4180a93b210665903b5c305a511936152866
MariaBarros02 <141078532+MariaBarros02@users.noreply.github.com>
2026-05-10 13:19:56 -0500

    Add: Ver error

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -3,16 +3,16 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export const registerRequest = (user) =>
   
-  axios.post(`${API_URL}/register`, user, {
+  axios.post(`${API_URL}/api/register`, user, {
     withCredentials: true,
   });
 
 export const loginRequest = (user) =>
-  axios.post(`${API_URL}/login`, user, {
+  axios.post(`${API_URL}/api/login`, user, {
     withCredentials: true,
   });
 
 export const logoutRequest = (user) =>
-  axios.post(`${API_URL}/logout`, user, {
+  axios.post(`${API_URL}/api/logout`, user, {
     withCredentials: true,
   });
